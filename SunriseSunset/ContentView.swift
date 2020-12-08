f9c59d213013b1d37af48c6d3ec5fd6b05b6dbd2
Sia <zyang@lcs.on.ca>
2020-12-08 14:07:23 -0500

    Initial Commit

---
+++ SunriseSunset/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SunriseSunset
+//
+//  Created by Zixi Yang on 2020-12-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SunriseSunset/SunriseSunsetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunriseSunsetApp.swift
+//  SunriseSunset
+//
+//  Created by Zixi Yang on 2020-12-08.
+//
+
+import SwiftUI
+
+@main
+struct SunriseSunsetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
